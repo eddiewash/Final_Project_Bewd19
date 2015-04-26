d1525b223be22357db9b3262f194f4e8aa0f9683
Melissa Gaines <gaines.melissa23@gmail.com>
2015-04-26 15:46:42 -0400

    kinda gettin this

--- final_project/app/models/archive.rb
+++ final_project/app/models/archive.rb
@@ -2,4 +2,7 @@ class Archive < ActiveRecord::Base
 
   belongs_to :category
   belongs_to :user
+
+
+
 end
--- final_project/app/models/category.rb
+++ final_project/app/models/category.rb
@@ -1,2 +1,13 @@
 class Category < ActiveRecord::Base
+
+  has_many :archives
+
+  def recent_archives
+    archive.limit(10)
+  end
+
+def self.from_params(category)
+    where(name: category.capitalize).first_or_create
+  end
+
 end
