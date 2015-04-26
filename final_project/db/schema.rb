598db544b610be021036b2f43a1b458481cec1ee
Ashley McCollum <ashleymccollum@Ashleys-MacBook-Pro.local>
2015-04-26 14:59:09 -0400

    ashley finally did something

--- final_project/db/schema.rb
+++ final_project/db/schema.rb
@@ -14,9 +14,13 @@
 ActiveRecord::Schema.define(version: 20150426175724) do
 
   create_table "archives", force: :cascade do |t|
-    t.string   "text"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "category"
+    t.string   "title"
+    t.string   "location"
+    t.string   "link"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "category_id"
   end
@@ -36,25 +40,13 @@ ActiveRecord::Schema.define(version: 20150426175724) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "batmen", force: :cascade do |t|
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "categories", force: :cascade do |t|
-    t.string   "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "harry_potter_books", force: :cascade do |t|
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "harry_potters", force: :cascade do |t|
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
@@ -97,21 +89,13 @@ ActiveRecord::Schema.define(version: 20150426175724) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
+    t.string   "email",               default: "", null: false
+    t.string   "encrypted_password",  default: "", null: false
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
